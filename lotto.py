f26aa94b15f017165cd3b32031b9f891fc175dc1
Liz Robson <lizziearobson@gmail.com>
2022-02-28 20:39:51 +0000

    lotto numbers using set

--- lotto.py
+++ lotto.py
@@ -2,5 +2,12 @@
 import random
 # numgenerator = (random.randint(1, 50))
 
-lotto_numbers = [random.randint(1, 50) for i in range(6)]
+# lotto_numbers = [random.randint(1, 50) for i in range(6)]
+# print(lotto_numbers)
+#
+#
+
+lotto_numbers = set()
+while len(lotto_numbers) < 6:
+    lotto_numbers.add(random.randint(1, 50))
 print(lotto_numbers)
